e65d2a36b18bba8fe43db30b7f294301206fbdf5
kutin9965 <157504182+kutin9965@users.noreply.github.com>
2024-08-02 17:59:01 +0700

    Create hardhat.config.ts

---
+++ hardhat.config.ts
@@ -0,0 +1,46 @@
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
+
+require('dotenv').config();
+
+module.exports = {
+  
+  solidity: {
+    version: "0.8.20", 
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: [process.env.PRIVATE_KEY || ""]
+    }, 
+  },
+
+  etherscan: {
+    // apiKey: `ANY_STRING_WILL_DO`,
+    apiKey:{
+      swisstronik:"any_string_will_do"
+    },
+    customChains: [
+      {
+        network: "swisstronik",
+        chainId: 1291,
+        urls: {
+          apiURL: "https://explorer-evm.testnet.swisstronik.com/api",
+          browserURL: "https://explorer-evm.testnet.swisstronik.com",
+        },
+      },
+    ],
+  },
+
+  sourcify: {
+    enabled: false,
+  }
+
+};
+
